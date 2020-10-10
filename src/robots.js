0b597c604afe5970140b03849aabcd6f2360fc73
FabricioRubo <48064337+FabricioRubo@users.noreply.github.com>
2020-10-10 16:02:24 -0300

    Updating the last changes

---
+++ src/robots.js
@@ -0,0 +1,69 @@
+export const robots = [
+    {
+      id: 1,
+      name: "Brendan O'brien",
+      username: 'Irish Man',
+      email: 'brendan.obrien@meta.com.br'
+    },
+    {
+      id: 2,
+      name: 'Emanuel Scherer',
+      username: 'Emanuel',
+      email: 'emanuel.scherer@meta.com.br'
+    },
+    {
+      id: 3,
+      name: 'Maksine Baldin',
+      username: 'Musikinha',
+      email: 'maksine.baldin@meta.com.br'
+    },
+    {
+      id: 4,
+      name: 'Leonardo Brum',
+      username: 'Brum',
+      email: 'leonardo.brum@meta.com.br'
+    },
+    {
+      id: 5,
+      name: 'Nicolas Araújo',
+      username: 'Martelinho de Ouro',
+      email: 'nicolas.araujo@meta.com.br'
+    },
+    {
+      id: 6,
+      name: 'João Larrosa',
+      username: 'Martin Luther King',
+      email: 'joao.larrosa@meta.com.br'
+    },
+    {
+      id: 7,
+      name: 'Eric Hertzog',
+      username: 'Eric',
+      email: 'eric.hertzog@meta.com.br'
+    },
+    {
+      id: 8,
+      name: 'Cláudio Pose',
+      username: 'Cláudio',
+      email: 'claudio.pose@meta.com.br'
+    },
+    {
+      id: 9,
+      name: 'Thiago Rosa',
+      username: 'Viking Pretended',
+      email: 'thiago.rosa@meta.com.br'
+    },
+    {
+      id: 10,
+      name: 'Fabrício Machado',
+      username: 'Eu',
+      email: 'fabricio.machado@meta.com.br'
+    },
+    {
+        id: 11,
+        name: 'Rômulo Monteiro',
+        username: 'Roteiro',
+        email: 'romulo.monteiro@meta.com.br'
+      }
+  ];
+  
